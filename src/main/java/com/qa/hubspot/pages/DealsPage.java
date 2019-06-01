db331871d77e0de8a749affd794642539b1ac5d4
learningjava1 <akilchoudhury@gmail.com>
2019-06-01 19:56:17 -0400

    added driver initialization part

--- src/main/java/com/qa/hubspot/pages/DealsPage.java
+++ src/main/java/com/qa/hubspot/pages/DealsPage.java
@@ -1,10 +1,17 @@
 package com.qa.hubspot.pages;
 
+import org.openqa.selenium.WebDriver;
+
 import com.qa.hubspot.base.BasePage;
 
 public class DealsPage extends BasePage{
 	
-	public DealsPage() {
+	WebDriver driver;
+	public DealsPage(WebDriver driver) {
+		this.driver=driver;
+		
+		
+		
 		
 	}
 	
